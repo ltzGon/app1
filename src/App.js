559a1871bcf0665fc1a0b9bee161d8660032d772
Gonzlzq <ggggggorn@gmail.com>
2024-08-08 13:15:23 +0700

    commit App.js
    commit App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import Calendar from './class-components'
+import { Header, Content, Footer } from './func-components'
 
 export default function App(){
   return(
     <>
-      <p><center><Calendar/></center></p>
+    <Header/>
+    <p><center><Calendar/></center></p>
+    <Content/>
+    <Footer/>
     </>
   )
 }

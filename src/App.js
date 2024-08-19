f1fcccc7935896d51b178afa056f4b411671a85c
Gonz <ggggggorn@gmail.com>
2024-08-19 10:28:53 +0700

    commit
    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React from 'react'
-import { Header, Content, Footer } from './func-components'
+import React from 'react';
+import RefsFunc from './refs-func'
 
-export default function App(){
+export default function App() {
   return(
-    <>
-    <Header/>
-    <p><center><Calendar/></center></p>
-    <Content/>
-    <Footer/>
-    </>
+    <RefsFunc/>
   )
+    
 }

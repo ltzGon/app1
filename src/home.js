c34d9df4bfc94dbf63dbae2e500b7cd824b8c96f
Gonz <ggggggorn@gmail.com>
2024-09-23 11:48:56 +0700

    404 commit

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <p style={{textAlign: 'center'}}>
+            <h3>ยินดีต้อนรับเข้าสู่ React Store</h3>
+            รายชื่อ <a href="/products">สินค้า</a> ที่เรามีจำหน่าย
+            <br/>
+            แต่ท่านต้องเป็น <a href="/member">สมาชิก</a>จึงจะสั่งซื้อได้ <br/>
+            หากมีข้อสงสัย <a href="/member">ติดต่อเรา</a>
+        </p>
+    )
+}
---
+++ src/member.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Router3(){
+    return (
+        <div style={{textAlign:'center'}}>Member Page</div>
+    )
+}

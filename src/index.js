70af5a209e92b8d614079900252f05e5dbe08975
Gonz <ggggggorn@gmail.com>
2024-09-30 11:40:31 +0700

    Boostrap commit
    Boostrap commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'boostrap/dist/css/boostrap.min.css'
+import 'boostrap/dist/js/boostrap.bundle.min.js'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
